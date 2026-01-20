41bb311827d18ef8572d56a804f8b6fe6d3c583d
zombizz <thibaultribes.tr@gmail.com>
2026-01-20 10:33:03 +0100

    Initial commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import axios from 'axios'
+
+axios.get('http://localhost:8000/api/test')
+    .then(res => {
+        console.log(res.data)
+    })
+</script>
+
+<template>
+  <h1>You did it!</h1>
+  <p>
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+  </p>
+</template>
+
+<style scoped></style>
