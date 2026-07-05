221b99ae7d996efe14e20d00b5d08729410c1a43
Zombizz97 <thibaultribes.tr@gmail.com>
2026-07-05 22:34:43 +0200

    feat (deploy): database

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 php artisan migrate --force
 
-php artisan tinker --execute='if (\App\Models\User::count() === 0) { echo "seeding..."; \Illuminate\Support\Facades\Artisan::call("db:seed", ["--force" => true]); }'
+php artisan db:seed --force
 
 php artisan storage:link
 apache2-foreground
